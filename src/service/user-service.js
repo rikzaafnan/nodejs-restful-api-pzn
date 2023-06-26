269eafb16e284b74369674283a1f03ab38830e9d
afnan <afnan@fastratabuana.co.id>
2023-06-26 14:08:14 +0700

    add fitur register user

--- src/service/user-service.js
+++ src/service/user-service.js
@@ -1,34 +1,39 @@
-import { prismaClient } from "../application/database";
-import { ResponseError } from "../error/response-error";
-import { registerUserValidation } from "../validation/user-validation"
-import { validate } from "../validation/validation"
+import {validate} from "../validation/validation.js";
+import {
+    registerUserValidation,
+} from "../validation/user-validation.js";
+import {prismaClient} from "../application/database.js";
+import {ResponseError} from "../error/response-error.js";
+import bcrypt from "bcrypt";
 
 const register = async (request) => {
     const user = validate(registerUserValidation, request);
 
-    const countUser  = await prismaClient.user.count({
+    const countUser = await prismaClient.user.count({
         where: {
             username: user.username
         }
     });
 
-
     if (countUser === 1) {
-            throw new ResponseError(400, "Username already exists")
+        throw new ResponseError(400, "Username already exists");
     }
-
 
     user.password = await bcrypt.hash(user.password, 10);
 
-    const result = await prismaClient.user.create({
-        data:user,
+    return prismaClient.user.create({
+        data: user,
         select: {
-            username:true,
-            name:true
+            username: true,
+            name: true
         }
-    })
+    });
+}
 
 
-    return result
 
+
+
+export default {
+    register
 }
